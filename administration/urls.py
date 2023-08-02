d8bd5b21f0f2d440535d1af340347faef7b6a996
Ajay peraboina <ajayperaboina@gmail.com>
2023-08-02 12:19:33 +0530

    second

--- administration/urls.py
+++ administration/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from admissions import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('newadm/',views.addadmission),
+    path('admreport/',views.admissionreport),
+    
+
 ]
